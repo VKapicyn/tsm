c547f87144e01b6361f2f2e8bfbeab604c036c8b
Vladislav <zveronym@gmail.com>
2017-11-08 20:42:07 +0300

    main page

---
+++ js/init.js
@@ -0,0 +1,29 @@
+var options = [
+  {selector: '#consalt', offset: 200, callback: function() {
+    $('#content1').show(600); }
+  },
+  {selector: '#trening', offset: 200, callback: function() {
+    $('#content2').show("slow");} 
+  },
+  {selector: '#issled', offset: 200, callback: function() {
+    $('#content3').show("slow");} 
+  },
+  {selector: '#izdat', offset: 200, callback: function() {
+    $('#content4').show("slow");} 
+  }
+];
+Materialize.scrollFire(options);
+
+(function($){
+  $(function(){
+
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav();
+    //$('#consalt').show("slow");
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
