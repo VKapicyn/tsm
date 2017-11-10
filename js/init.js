a2a5c893fce47fcedd37340d934f5f59a563f3fe
Vladislav <zveronym@gmail.com>
2017-11-10 19:03:30 +0300

    release 0.0.9

--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 var options = [
+  //index
   {selector: '#consalt', offset: 200, callback: function() {
     $('#content1').show(600); }
   },
@@ -16,6 +17,19 @@ var options = [
   },
   {selector: '.parallax', offset: 200, callback: function() {
     $('#general').slideDown("slow");} 
+  },
+  //izdat
+  {selector: '#izdat1', offset: 200, callback: function() {
+    $('.content1').fadeIn(2000);} 
+  },
+  {selector: '#izdat2', offset: 200, callback: function() {
+    $('.content2').fadeIn(2000);} 
+  },
+  {selector: '#izdat3', offset: 200, callback: function() {
+    $('.content3').fadeIn(2000);} 
+  },
+  {selector: '#izdat4', offset: 200, callback: function() {
+    $('.content4').fadeIn(2000);} 
   }
 ];
 Materialize.scrollFire(options);
